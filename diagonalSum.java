ee9d236270366d82d5b739056057ea8b8a795f74
Hariom Singh Thakur <126336846+hariom-hub@users.noreply.github.com>
2024-01-29 10:34:16 +0530

    Create diagonalSum.java

---
+++ diagonalSum.java
@@ -0,0 +1,22 @@
+public class diagonalSum {
+
+    public static int DiagonalSum(int[][] matrix) {
+
+        int sum = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+          public class diagonalSum {
+
+    public static int DiagonalSum(int[][] matrix) {
+
+        int sum = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+             // time complexity = O(n^2)
+    public static void main(String[] args) {
+
+        int[][] matrix = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+        System.out.println("sum is : " + DiagonalSum(matrix));
+
+    }
+}
