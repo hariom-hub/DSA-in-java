7692f77e3aa9bf6d7633e8ff0578403f35b155a9
Hariom Singh Thakur <126336846+hariom-hub@users.noreply.github.com>
2024-02-04 17:36:42 +0530

    Create checkSorted.java

---
+++ checkSorted.java
@@ -0,0 +1,24 @@
+public class checksorted {
+
+    public static boolean isSorted(int []arr,int i){
+
+        if(i == arr.length-1){
+            return true;
+        }
+
+        if(arr[i] > arr[i+1]){
+            return false;
+        }
+
+        return isSorted(arr,i+1);
+    }
+
+    public static void main(String[] args){
+
+        int []arr = {1,2,3,4,5};
+        int []array = {2,3,5,1,6,4};
+        System.out.println(isSorted(array,0));
+        System.out.println(isSorted(arr,0));
+
+    }
+}
